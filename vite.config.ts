b1868c422f1de348f7d81912093ffce70bace59c
shuami-dev <shuami-dwv@gmail.com>
2025-01-16 09:17:42 +0800

    Update package

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 		lib: {
 			// Entry point for the library
 			entry: './src/index.ts',
-			// name: '@shuami-dev/nuxt-session-i18n-theme',
+			name: '@shuami-dev/nuxt-session-i18n-theme',
 			// Generate both ES Module and CommonJS formats
 			formats: ['es', 'cjs'],
 			// ES module will output as .esm.js, CommonJS as .cjs.js
